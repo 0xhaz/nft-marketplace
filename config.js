190e9236350ab712587cdfb9f819c56bc1b25701
0xhaz <28639279+0xhaz@users.noreply.github.com>
2022-03-26 21:18:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export const nftaddress = "0xf963c8ed3e4ad9ed87dfe3c97022f29618690387"
 export const nftmarketaddress = "0x72c7c0c2a3dacdc1fcbe2f3fabc2450b10cc8c85"
 
-import nftabi from "./src/artifacts/contracts/NFT.sol/NFT.json"
-import nftMarketabi from "./src/artifacts/contracts/NFTMarket.sol/NFTMarket.json"
+// import nftabi from "./src/artifacts/contracts/NFT.sol/NFT.json"
+// import nftMarketabi from "./src/artifacts/contracts/NFTMarket.sol/NFTMarket.json"
 
 export const nftABI = nftabi.abi;
 export const nftMarketABI = nftMarketabi.abi;
